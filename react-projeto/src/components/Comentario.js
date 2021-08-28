bde179827db919985e2002e171ab54ba5a7ce805
Afonso-cmd <afonso.ur@gmail.com>
2021-08-28 17:17:05 -0300

    componets

---
+++ react-projeto/src/components/Comentario.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Comentario.css'
+
+//jsx
+//one div for one components
+const Comentario = () => (
+    <div className="Classe">Teste Componente</div>
+);
+
+export default Comentario;
