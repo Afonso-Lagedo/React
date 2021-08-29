521ca91b5ac9e1c843298b80d7b7e123bbe05499
Afonso-cmd <afonso.ur@gmail.com>
2021-08-29 19:13:25 -0300

    handle events

--- react-projeto/src/App.js
+++ react-projeto/src/App.js
@@ -21,25 +21,47 @@ class App extends Component{//class have function, function have return
         data:new Date (2021,8,29),
         mensagem:'Teste comentário2'
       }
-    ]
+    ],
+    novoComentario:{
+      nome:'',
+      email:'',
+      mensagem:''
+    }
   }
 
-  adicionarComentario = () =>{
+  adicionarComentario = evento =>{
+
+    evento.preventDefault();//for event annull
+
     console.log("Adicionando Comentário...");
 
-    const novoComentario ={
-        nome:'Rafaela',
-        email:'rafaela@gmail.com',
-        data:new Date (),
-        mensagem:'Teste comentário3'
-    }
+    //const novoComentario ={
+    //    nome:'Rafaela',
+    //    email:'rafaela@gmail.com',
+    //    data:new Date (),
+    //    mensagem:'Teste comentário3'
+    //}
+    //pick up from the state:
 
-    //let lista = this.state.comentarios;//geting comentarios
-    //lista.push(novoComentario);//add comentario
-    //this.setState({comentarios:lista})//state update
+    const novoComentario = {...this.state.novoComentario, data: new Date()};//pick up from state of novoComentario plus actualy date
 
-    //or with sprad
-    this.setState({comentarios: [...this.state.comentarios, novoComentario]})
+    this.setState({comentarios: [...this.state.comentarios, novoComentario],
+    novoComentario: {nome:'', email:'', mensagem:''}//update field, after add
+    })
+  }
+
+  editaInput = evento => {
+    //console.log(evento); //for start event
+    //console.log(evento.target);//who started the event
+    //console.log(evento.target.value);//what is being typed
+
+    //geting value and name
+    /*const value = evento.target.value;
+    const name = evento.target.name;
+    or ...*/
+    const {name, value} = evento.target;
+    //field update with value
+    this.setState({novoComentario: {...this.state.novoComentario, [name]:value}});
   }
 
   render() {//from library Component
@@ -48,16 +70,51 @@ class App extends Component{//class have function, function have return
         <h1>Comentários</h1>
         {
           this.state.comentarios.map((comentario, indice)=>(
-            <Comentario 
+            <Comentario
               key={indice}//each components have identification unique 
-              nome={comentario.nome} 
-              email={comentario.email} 
+              nome={comentario.nome}
+              email={comentario.email}
               data={comentario.data}>
               {comentario.mensagem}
             </Comentario>
           ))
         }
-        <button onClick={this.adicionarComentario}>Adicionar comentário</button>
+
+        <form method="post" onSubmit={this.adicionarComentario}>
+          <h2>Adicionar Comentário</h2>
+          <div>
+            <input
+              type="text"
+              name="nome"
+              value={this.state.novoComentario.nome}
+              onChange={this.editaInput}
+              placeholder="Digite seu nome"
+            />
+          </div>
+
+          <div>
+            <input
+              type="email"
+              name="email"
+              value={this.state.novoComentario.email}
+              onChange={this.editaInput}
+              placeholder="Digite seu email"
+            />
+          </div>
+
+          <div>
+            <textarea
+              name="mensagem"
+              value={this.state.novoComentario.mensagem}
+              onChange={this.editaInput}
+              rows="4"
+            />
+          </div>
+
+          <button type="submit">Adicionar comentário</button>
+
+        </form>
+
       </div>
     );
   }
